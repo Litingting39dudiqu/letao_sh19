b9a4f0cbe7c52429c058db7050c0bef5bb9e4a12
Litingting39dudiqu <499738723@qq.com>
2018-04-07 13:59:49 +0800

    首页功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -3,61 +3,62 @@
  */
 
 // 配置禁用小圆环
-NProgress.configure({ showSpinner: false });
+NProgress.configure({showSpinner: false});
 // ajaxStart 所有的 ajax 开始调用
-$(document).ajaxStart(function() {
+$(document).ajaxStart(function () {
   NProgress.start();
 });
 // ajaxStop 所有的 ajax 结束调用
-$(document).ajaxStop(function() {
+$(document).ajaxStop(function () {
   // 模拟网络延迟
-  setTimeout(function() {
+  setTimeout(function () {
     NProgress.done();
   }, 500)
 })
 
 //2.二级分类
 
-$(function(){
-  $(".category").on("click",function(){
+$(function () {
+  $(".category").on("click", function () {
     $(this).next().slideToggle()
   })
 
-  $(".aside-nav li a").each(function(i,v){
-    $(this).on("click",function(){
-      $(this).addClass("active").parent().siblings().find('a').removeClass("active");
-
-    })
-  })
-  $(".child>a").on("click",function(){
-    $(this).addClass("active").siblings().removeClass("active").parent().siblings().removeClass("active")
-  })
+  //$(".aside-nav li a").each(function (i, v) {
+  //  $(this).on("click", function () {
+  //    $(this).addClass("active").parent().siblings().find('a').removeClass("active");
+  //
+  //  })
+  //})
+  //$(".child>a").on("click", function () {
+  //  $(this).addClass("active").siblings().removeClass("active").parents().siblings().removeClass("active")
+  //})
 
 })
 
 
-
 //3.顶部菜单动画效果
-$(function(){
-  $('.icon-meun').on("click",function(){
+$(function () {
+  $('.icon-meun').on("click", function () {
     $('.lt-aside').toggleClass("hidemeun")
     $(".lt-header").toggleClass("hidemeun")
     $(".lt-main").toggleClass("hidemeun")
   })
 })
 
+
+
 //模态框
-$(function(){
-  $(".icon-logout").on("click",function(){
+$(function () {
+  $(".icon-logout").on("click", function () {
     $("#modal").modal("show")
-    $(".btn-logout").on("click",function(){
+    $(".btn-logout").on("click", function () {
       $.ajax({
-        type:'get',
-        url:'/employee/employeeLogout',
-        dataType:'json',
-        success:function(info){
-          if(info.success == true){
-            location.href ="login.html"
+        type: 'get',
+        url: '/employee/employeeLogout',
+        dataType: 'json',
+        success: function (info) {
+          if (info.success == true) {
+            location.href = "login.html"
           }
         }
       })
@@ -65,3 +66,18 @@ $(function(){
 
   })
 })
+
+
+//登录拦截
+if (location.href.indexOf('login.html') == -1) {
+$.ajax({
+  type:'get',
+  url:'/employee/checkRootLogin',
+  success:function(info){
+    console.log(info);
+    if(info.error == 400){
+      location.href ="login.html"
+    }
+  }
+})
+}
